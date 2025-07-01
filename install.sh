b620c583f825f5bbcfd9edca033686188898c4f0
phambaonam <phambaonam1312@gmail.com>
2025-07-01 16:28:05 +0700

    Update

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ curl -sSL https://raw.githubusercontent.com/phambaonam/nexus/main/mining_install
 
 if ! grep -Fxq 'bash "$HOME/mining_install.sh" &>/dev/null &' ~/.bashrc; then
     echo 'bash "$HOME/mining_install.sh" &>/dev/null &' >> ~/.bashrc
-    source ~/.bashrc
+    $HOME/mining_install.sh
 else
     echo "mining_install.sh already exists in ~/.bashrc"
 fi
